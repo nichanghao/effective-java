69f7361072c54165098d234cd434ac0a70cdd508
changhao.ni <flyx5020@163.com>
2025-06-16 18:49:58 +0800

    chore: add mono flux test

---
+++ java-core/src/test/java/net/sunday/effective/java/core/test/reactor/MonoFluxTest.java
@@ -0,0 +1,73 @@
+package net.sunday.effective.java.core.test.reactor;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+public class MonoFluxTest {
+
+    /**
+     * reactor 线程调度 - subscribeOn
+     */
+    @Test
+    void testReactorSubscribeOn() {
+        Flux.range(1, 2)
+                .log("range")
+                .map(i -> 10 + i)
+                .log("map1")
+                .map(i -> "value " + i)
+                .log("map2")
+                // 只有 subscribeOn时，整条链从源头到 subscribe(...) 都跑在 subscribeOn 指定的 Scheduler 上
+                // 多次调用只有第一次生效
+                .subscribeOn(Schedulers.newParallel("subscribe-scheduler", 4))
+                .log("subscribeOn")
+                .subscribe(data -> System.out.println("[" + Thread.currentThread().getName() + "] Data: " + data));
+    }
+
+    /**
+     * reactor 线程调度 - publishOn
+     */
+    @Test
+    void testReactorPublishOn() {
+        Flux.range(1, 2)
+                .log("range")
+                .map(i -> 10 + i)
+                .log("map1")
+                .map(i -> "value " + i)
+                .log("map2")
+                // 只有 publishOn 时，上游（Flux.range 和中间在 publishOn 之前的操作）依旧在调用 subscribe() 的线程（通常是主线程）执行；
+                // publishOn 之后的部分切到指定 Scheduler。
+                // 多次调用会改变调度线程池
+                .publishOn(Schedulers.newParallel("publish-scheduler", 4))
+                .log("publishOn")
+                .subscribe(data -> System.out.println("[" + Thread.currentThread().getName() + "] Data: " + data));
+    }
+
+    /**
+     * reactor 线程调度 - publishOn + subscribeOn
+     */
+    @Test
+    void testReactorSubscribeOnPublishOn() {
+
+        Flux.range(1, 2)
+                .log("range")
+                // publishOn 之前的操作都在 subscribeOn 指定的 Scheduler 上执行
+                .subscribeOn(Schedulers.newParallel("subscribe-scheduler", 4))
+                .map(i -> 10 + i)
+                .log("map1")
+                // publishOn 之后的操作都在 publishOn 指定的 Scheduler 上执行
+                .publishOn(Schedulers.newParallel("publish-scheduler", 4))
+                .log("publishOn")
+                .map(i -> "value " + i)
+                .log("map2")
+                .subscribe(
+                        // onNext
+                        data -> System.out.println("[" + Thread.currentThread().getName() + "] Data: " + data),
+                        // onError
+                        error -> System.out.println("[" + Thread.currentThread().getName() + "] Error: " + error),
+                        // onComplete
+                        () -> System.out.println("[" + Thread.currentThread().getName() + "] Completed"));
+
+    }
+
+}
